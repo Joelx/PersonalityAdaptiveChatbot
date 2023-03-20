56a06a080e8ccd0a4e0363aa2748805fd77a4039
Joelx <joel.schlotthauer@gmail.com>
2023-03-20 19:46:30 +0100

    added panel iframe to website

--- actions/actions.py
+++ actions/actions.py
@@ -6,6 +6,7 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+import re
 from typing import Any, Text, Dict, List
 #
 from rasa_sdk import Action, Tracker
@@ -21,6 +22,11 @@ class ActionHaystack(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        # Check if a form is running. If so, we want rasa to handle it without junking up our conversation data.
+        active_loop = tracker.active_loop.get('name')
+        if active_loop:
+            print(active_loop)
+            return
         conversation = self._parse_rasa_events_to_conversation(tracker.events)
         url = "http://10.0.1.1:8001/query"
 
@@ -59,5 +65,11 @@ class ActionHaystack(Action):
             for event in events:
                 conversation_type = conversation_map.get(event["event"])
                 if conversation_type:
+                    # If its only a number (e.g. from Big 5 test), we dont want it
+                    if event["text"].isdigit():
+                        continue
+                    # Likewise we want to filter out potential Big Five questions that have been asked
+                    if re.match("Aussage \d+/\d+", event["text"]):
+                        continue
                     conversation.append({"event": conversation_type, "message": event["text"]})    
         return conversation
